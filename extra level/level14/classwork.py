f79522c6320790217f420ef86284abeca097a0df
Giorgi-gurieli <giorgigurieli14@gmail.com>
2024-10-22 21:08:31 +0400

    first commit

---
+++ extra level/level14/classwork.py
@@ -0,0 +1,47 @@
+#for i in range(10):
+    #print(i)
+
+#for i in range(3, 10, 4):
+    #print(i)
+
+#i = 0
+#while 3 > i:
+    #print("group 47")
+
+        # i = i + 1
+        # i += 1
+
+#name = "luka"
+#name = "davit"
+
+#print( name )
+
+
+#name = "davit"
+
+#for i in name:
+    #print(i)
+#for i in "lomi":
+    #print(i)
+
+#name = "two tower"
+#result = ""
+#result += name
+#for i in name:
+    #result += i
+    #print(result)
+#           True/ False
+# if statment/ მოქმედება ---> ყოველთვის უნდა შესრულდეს  True
+
+
+if 4 > 1 :
+    print("მამა აფრიკა")
+elif 4 > 3: # else if ----> elif True უნდა გადაცდეს
+    print("4 > 3 რა უჭირს მერე")
+elif 4 > 3 : 
+    #გამოგვიტანს შედეგს
+    print("4 > 3 რა უჭირს მერე")
+else :# STATMENT  არ გააჩნია და False ის შემთხვევაში 
+    print(" ეს პირობა არ შესრულდა")
+
+
