2943a25b5859a814075dc424af9114dd668a9960
Giorgi-gurieli <giorgigurieli14@gmail.com>
2024-12-16 18:37:18 +0400

    comment

---
+++ level04/hw/hw4.py
@@ -0,0 +1,24 @@
+# 1
+name = input("enter your name ")
+print(name)
+last_name = input("last name ")
+print(last_name)
+age = input("age ")
+print(age)
+
+# 2
+
+num1 = int(input("enter number "))
+num2 = int(input("enter number "))
+print(num1 * num2)
+
+# 3
+
+academy = input("where do you go to university? ")
+school = input("where do you go to school? ")
+name = input("enter your name ")
+last_name = input("last name ")
+print(academy)
+print(school)
+print(name)
+print(last_name)
--- level15/cw/cw15.py
+++ level15/cw/cw15.py
@@ -1,1 +1,51 @@
-# no classwork
+# res = ""
+# for i in "davit"
+
+# while True:
+
+# if 2>2: false
+#   print("araferi")
+# elif 2>3 false
+# else:
+#   print("lomi")
+
+
+# if 3 >= 3:
+#   print("start")
+# print("end")
+age = 16
+if age < 14: print("discount")
+print("lado magaria")
+
+# list ---> data type     | array | მასივი 
+# მასივებში | სიებში შეგვიძლია რამდენი მონაცებთა ტიპში გვინდა იმდენი ჩავაწეროთ
+# ერთმანეთიგან ისინი გამოიყოფიან ---->  , <----
+#               0         1          2     3    4
+giorgi_list = [ "vaja","daviti","daviti2", True 12 ]
+
+item1 = giorgi_list[0]
+res = ""
+for i in range(5):
+   res +=str(giorgi_list[i]) + ""
+print(res)
+
+# string ---- შეიძლია
+
+
+#       01234567
+#           -3-2-1
+name = "group 47"
+
+#               0         1         2      3       4
+#               -5       -4         -3     -2       -1
+giorgi_list = ["vaja","daviti","daviti2", True 12 "ლომი"]
+
+# თითოეული ელემენტის მნიშვნელობას დაგვიპრინტავს <3
+for element in giorgi_list:
+    print(element)
+
+
+print(giorgi_list[-1])
+
+# for index in range(5):
+    # print(giorgi_list[index])
